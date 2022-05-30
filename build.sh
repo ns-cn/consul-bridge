7b7c7022230dcabff55030a87c2ab1ea9347113d
ns-cn <ns-cn@qq.com>
2022-05-30 15:48:00 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./built/consul_bridge_linux .
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./built/consul_bridge_win.exe .
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./built/consul_bridge_darwin .
