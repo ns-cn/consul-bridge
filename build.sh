19d5c7fc91ea3d77834eb7a7091bfe11c7e99b13
ns-cn <ns-cn@qq.com>
2022-11-12 14:49:21 +0800

    1.0.9: 减少可执行体积

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./releases/consul_bridge_linux .
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./releases/consul_bridge_win.exe .
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./releases/consul_bridge_darwin .
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -o ./releases/consul_bridge_linux .
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-w -s" -o ./releases/consul_bridge_win.exe .
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-w -s" -o ./releases/consul_bridge_darwin .
--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 	"github.com/hashicorp/consul/api"
 )
 
-const VERSION = "1.0.8"
+const VERSION = "1.0.9"
 
 var targetSettingFile string
 
